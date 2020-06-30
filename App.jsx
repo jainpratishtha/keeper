3d66273e2c89819367bbad7c775fea21d29acc4a
jainpratishtha <49338769+jainpratishtha@users.noreply.github.com>
2020-06-30 21:32:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Note from "./note";
+import Footer from "./footer";
+import Header from "./header";
+
+function App(){
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+
+    </div>
+}
+export default App;
---
+++ note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note(){
+return <div class="note"><h1>heading</h1>
+<br/>
+<p>
+    content
+</p>
+</div>;
+}
+export default Note;
